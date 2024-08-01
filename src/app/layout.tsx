ca0e25de60d188314ef7974f2c4f3104271297e8
Tyler <tyler@tyler.love>
2024-08-01 15:43:11 +0200

    icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next"
-import Head from "next/head"
 import "./globals.css"
 
 export const metadata: Metadata = {
@@ -16,6 +15,19 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/swx0tcr.css" />
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://mirage.space"/>
+        <meta property="og:title" content="La Mirage"/>
+        <meta property="og:image" content="https://mirage.space/icon.png"/>
+        <meta property="og:image:width" content="1200"/>
+        <meta property="og:image:height" content="1200"/>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta property="og:description" content="La Mirage is a band from Lenny + Tyler <3"/>
+        <meta name="description" content="La Mirage is a band from Lenny + Tyler <3"/>
+        <meta property="og:site_name" content="La Mirage"/>
+        <link rel="icon" href="/favicon.ico"/>
+        <link rel="apple-touch-icon" href="/icon.png"/>
+        <meta name="robots" content="max-image-preview:large"/>
       </head>
       <body>{children}</body>
     </html>
