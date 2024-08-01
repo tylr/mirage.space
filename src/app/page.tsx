9a4682712b900687ba8632c62fc969e98c652acf
Tyler <tyler@tyler.love>
2024-08-01 15:21:20 +0200

    hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <header>
-      <h1 className="title">La Mirage</h1>
+      <h1 className="title">La<br />Mirage</h1>
     </header>
   );
 }
